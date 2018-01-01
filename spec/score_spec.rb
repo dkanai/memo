740b524debc22f81b032f9d426a0bb48c1e1f70f
d.kanai <d.kanai@pastoraldog.com>
2018-01-01 10:46:46 +0900

    Revert "Revert "Refactor stdout""
    This reverts commit 7ba54503a943d275aa16bb35ed080847ee91d279.

--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -11,7 +11,7 @@ describe 'run' do
   it do
     @operation = 1
     allow(StdIn).to receive(:gets).and_return('10') 
-    expect(subject).to eq ">Successfully created score\n"
+    expect(subject).to eq 'Successfully created score'
   end
   it do
     @operation = 2
@@ -20,11 +20,11 @@ describe 'run' do
   it do
     allow(StdIn).to receive(:gets).and_return('1') 
     @operation = 3
-    expect(subject).to eq ">Successfully deleted score\n"
+    expect(subject).to eq 'Successfully deleted score'
   end
   it do
     ScoreFile.append("0")
     @operation = 4
-    expect(subject).to eq ">Score Average: 50.0"
+    expect(subject).to eq 'Score Average: 50.0'
   end
 end
