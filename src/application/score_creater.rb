ab78eb89ccb8525f49a290e2f436a6f9ac966489
d.kanai <d.kanai@pastoraldog.com>
2018-01-01 17:21:30 +0900

    Revert "Refactor remove primitive obsession. amazing close to rails..."
    This reverts commit 9c3fa5637a52dc335bdadff0964df744fdbd1861.

--- src/application/score_creater.rb
+++ src/application/score_creater.rb
@@ -9,12 +9,10 @@ class ScoreCreater
 
   def run
     StdOut.print('Please enter the score')
-    score_file_line = ScoreFileLine.new(StdIn.gets)
-    if score_file_line.save
-      ScoreOperatorResult.new('success', 'Successfully created score')
-    else
-      ScoreOperatorResult.new('error', score_file_line.validator.errors.join(''))
-    end
+    input = StdIn.gets
+    return ScoreOperatorResult.new('error', @validator.errors.join('')) if @validator.set_value(input).invalid?
+    ScoreFile.append(input)
+    ScoreOperatorResult.new('success', 'Successfully created score')
   end
 
 end
